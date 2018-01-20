d14e728ec98e35c67b479c0b0a12a778c6a67e10
Huey <jasperjeng@gmail.com>
2018-01-20 14:46:31 -0800

    added backend files

--- src/components/Root.js
+++ src/components/Root.js
@@ -5,10 +5,30 @@ import '../styles/App.css';
 import GoogleMapContainer from "../containers/GoogleMapContainer";
 
 class Root extends Component {
+  state = {
+    response: '',
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
   render() {
     return (
       <div className="root">
         <GoogleMapContainer />
+        <p className="App-intro">{this.state.response}</p>
       </div>
     );
   }
