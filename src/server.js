89564159c9bf4158b7e0e9b82fb7f18a1583629b
huey <Huey Freeman>
2018-01-20 15:11:30 -0800

    m

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = 5000;
+const port = 2000;
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
