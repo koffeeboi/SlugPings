9d728628b131d2aa3ebbb3549c35cd1fed30a67c
huey <Huey Freeman>
2018-01-20 03:23:37 -0800

    move google into component

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,13 @@ import logo from '../assets/logo.svg';
 import '../styles/App.css';
 
 import Button from "../components/Button";
-import Map from "../components/Map";
+import GoogleMap from "../components/GoogleMap";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Map />
+        <GoogleMap />
       </div>
     );
   }
--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -3,7 +3,21 @@ import GoogleMapReact from 'google-map-react';
 
 import "../styles/GoogleMap.css";
 
-class Map extends Component {
+const AnyReactComponent = ({ text }) => (
+  <div style={{
+    position: 'relative', color: 'white', background: 'red',
+    height: 40, width: 60, top: -20, left: -30,
+  }}>
+    {text}
+  </div>
+);
+
+class GoogleMap extends Component {
+  static defaultProps = {
+    center: { lat: 59.95, lng: 30.33 },
+    zoom: 11
+  };
+
   constructor(props) {
     super(props);
   }
@@ -11,19 +25,24 @@ class Map extends Component {
   render() {
     return (
       <div className="map-container">
-      <GoogleMapReact
-        defaultCenter={this.props.center}
-        defaultZoom={this.props.zoom}
-      >
-        <AnyReactComponent
-          lat={59.955413}
-          lng={30.337844}
-          text={'Kreyser Avrora'}
-        />
-      </GoogleMapReact>
+        <GoogleMapReact
+          bootstrapURLKeys={{
+            key: "AIzaSyAbDBAOO2E-5onre2peQuHTcaOtn1dav6k",
+            language: 'en',
+            region: 'us',
+          }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text={'Place'}
+          />
+        </GoogleMapReact>
       </div>
     );
   }
 }
 
-export default Map;
+export default GoogleMap;
