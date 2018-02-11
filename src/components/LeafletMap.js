63d6b74d1402836a06b64491112e3f133cbaebd7
Huey <jasperjeng@gmail.com>
2018-02-11 14:58:27 -0800

    Working example code from react-leaflet

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 
 import "../styles/LeafletMap.css";
 
-import Marker from "../components/Marker";
 import { postMarkerToServer } from "../utils/utils";
 
-class LeaftletMap extends Component {
+const position = [36.9924,-122.0562]
+
+class LeafletMap extends Component {
   constructor(props) {
     super(props);
 
@@ -19,7 +21,19 @@ class LeaftletMap extends Component {
   render() {
     return (
       <div className="map-container">
-        
+        <div className="leafletmap">
+          <Map center={position} zoom={13}>
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+            />
+            <Marker position={position}>
+              <Popup>
+                <span>A pretty CSS3 popup.<br />Easily customizable.</span>
+              </Popup>
+            </Marker>
+          </Map>
+        </div>
       </div>
     );
   }
