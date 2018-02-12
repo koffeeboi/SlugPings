ca2ce280ddf53ba046b50c791dfc7bd0cc1a9123
Huey <jasperjeng@gmail.com>
2018-02-11 18:18:37 -0800

    Remove old file dependencies.

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../assets/logo.svg';
 import '../styles/App.css';
-import './modal.css';
-import './modal.js';
 
 import LeafletMapContainer from "../containers/LeafletMapContainer";
 
