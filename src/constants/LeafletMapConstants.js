fac7f58468ec5fd7d23f850298acee95ed86ab5a
Huey <jasperjeng@gmail.com>
2018-02-11 16:43:19 -0800

    Removed old GoogleMap constants.

--- src/constants/LeafletMapConstants.js
+++ src/constants/LeafletMapConstants.js
@@ -1,4 +1,1 @@
-export const MAP_SET_CURR_LOC = "MAP_SET_CURR_LOC";
-export const MAP_PUSH_MARKER = "MAP_PUSH_MARKER";
-export const MAP_SET_FROM_DB = "MAP_SET_FROM_DB";
-export const MAP_UPDATE_TEXT = "MAP_UPDATE_TEXT";
+export const MAP_ADD_MARKER = "MAP_ADD_MARKER";
