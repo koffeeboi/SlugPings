2fa7be5bdd12aed2dd291f318cbb5a9c98e8c612
Huey <jasperjeng@gmail.com>
2018-02-11 16:44:16 -0800

    Removed old state from GoogleMaps.

--- src/reducers/map.js
+++ src/reducers/map.js
@@ -1,63 +1,21 @@
 import * as types from "../constants/LeafletMapConstants";
 
 const initialState = {
-  currentLocClicked: {
-    x: 0,
-    y: 0,
-    lat: 0,
-    lng: 0,
-  },
-  markers: [{
-    x: 5,
-    y: 5,
-    lat: 12,
-    lng: 30,
-    text: "",
-  }],
-  db: null,
+  markers: [
+    {
+      lat: 36.99694,
+      lng: -122.05954,
+    },
+  ],
 };
 
 const map = (state = initialState, action) => {
   switch (action.type) {
-    case (types.MAP_SET_CURR_LOC): {
+    case (types.MAP_ADD_MARKER): {
+      console.log(state.markers);
       return {
         ...state,
-        currentLocClicked: { ...action.payload },
-      };
-    }
-
-    case (types.MAP_PUSH_MARKER): {
-      return {
-        ...state,
-        markers: [
-          ...state.markers,
-          action.payload,
-        ],
-      };
-    }
-
-    case (types.MAP_UPDATE_TEXT): {
-      let j;
-      for (let i = 0; i < state.markers.length; i++) {
-        if (state.markers[i].id == action.payload.id){
-          j = i; break;
-        }
-      }
-
-      let d = state.markers.splice(j, 1)[0];
-      d.text = action.payload.text;
-      
-      state.markers.push(d);
-      return {
-        ...state,
-        markers: [...state.markers],
-      }
-    }
-
-    case (types.MAP_SET_FROM_DB): {
-      return {
-        ...state,
-        db: action.payload,
+        markers: [...state.markers, action.payload],
       };
     }
 
