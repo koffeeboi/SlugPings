6c4d0ac6bbea26e936521d5affde287306b38451
Huey <jasperjeng@gmail.com>
2018-02-19 00:47:09 -0800

    Testing process.env

--- src/database/server.js
+++ src/database/server.js
@@ -1,9 +1,7 @@
 // var mongoose = require('mongoose');
 import Mongoose from "mongoose";
 
-console.log(Mongoose);
-
-var mongoDB = process.env.MLAB; console.log(mongoDB);
+var mongoDB = process.env.MLAB; console.log(process.env);
 var db = Mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once("open", () => {
