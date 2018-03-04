d46195a0e26bca18e953087a13d8d412ca65cdec
Huey <jasperjeng@gmail.com>
2018-03-03 20:20:00 -0800

    Added login and signup to main reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,16 @@ import { combineReducers } from 'redux';
 import sidebar from "../reducers/sidebar";
 import listings from "../reducers/listings";
 import chat from "../reducers/chat";
+import login from "../reducers/login";
+import signup from "../reducers/signup";
 import map from "../reducers/map";
 
 const rootReducer = combineReducers({
   sidebar,
   listings,
   chat,
+  login,
+  signup,
   map,
 });
 
