112b62f83d7dfa083a7b749d213f841f5eca2982
Huey <jasperjeng@gmail.com>
2018-03-04 00:09:53 -0800

    Keep only email and password

--- backend/model/user.js
+++ backend/model/user.js
@@ -8,20 +8,10 @@ var UserSchema = new mongoose.Schema({
     required: true,
     trim: true
   },
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true
-  },
   password: {
     type: String,
     required: true,
   },
-  passwordConf: {
-    type: String,
-    required: true,
-  }
 });
 
 //authenticate input against database
