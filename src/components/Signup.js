83f3874a606f5488f49ef9b009cb8df680ea42ea
Huey <jasperjeng@gmail.com>
2018-03-04 00:10:43 -0800

    Added sign up form and form validation

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -7,6 +7,7 @@ class Signup extends Component {
     super(props);
 
     this._onClose = this._onClose.bind(this);
+    this._onSubmit = this._onSubmit.bind(this);
   }
 
   componentsDidMount() {
@@ -18,13 +19,104 @@ class Signup extends Component {
     hideSignup();
   }
 
+  _onSubmit(e) {
+    e.stopPropagation();
+    if (!this.email.value.endsWith("@ucsc.edu")) {
+      console.log(`${this.email.value} does not end with @ucsc.edu`);
+      e.preventDefault();
+      return false;
+    }
+
+    if (this.password.value != this.verifyPassword.value) {
+      // console.log(`Passwords do not match: ${this.password.value} ${this.verifyPassword.value}`);s
+      e.preventDefault();
+      return false;
+    }
+  }
+
   render() {
     const { signup } = this.props;
     const { isSignupOpen } = signup;
 
     return !isSignupOpen ? null : (
-      <div className="signup">
-        Sign up form
+      <div
+        className="signup"
+      >
+        <div className="signup-content">
+          <div className="signup-header">
+            <span
+              className="close"
+              onClick={this._onClose}
+            >
+              &times;
+            </span>
+            <h2>Sign up</h2>
+          </div>
+          <div className="signup-body">
+            <form id="SignupForm" action="http://localhost:3001" method="POST">
+              <fieldset>
+                <input
+                  id="iEmail"
+                  type="text"
+                  name="email"
+                  placeholder="Email (@ucsc.edu)"
+                  ref={(input) => { this.email = input }}
+                />
+                <br />
+                <input
+                  id="iPassword"
+                  type="text"
+                  name="password"
+                  placeholder="Password"
+                  ref={(input) => { this.password = input }}
+                />
+                <br />
+                <input
+                  id="iVerifyPassword"
+                  type="text"
+                  name="verifyPassword"
+                  placeholder="Verify Password"
+                  ref={(input) => { this.verifyPassword = input }}
+                />
+                <br />
+              </fieldset>
+              <input form="SignupForm" type="submit" onClick={this._onSubmit} />
+            </form>
+            {/* <div className="input-container">
+              <input
+                type="text"
+                className="form-input"
+                id="addTitle"
+                placeholder="Title"
+                ref={(input) => { this.title = input }}
+              />
+            </div>
+            <input
+              type="text"
+              className="form-input"
+              id="addTitle"
+              placeholder="username"
+              ref={(input) => { this.title = input }}
+            />
+          </div> <div className="input-container">
+            <input
+              type="text"
+              className="form-input"
+              id="addTitle"
+              placeholder="password"
+              ref={(input) => { this.title = input }}
+            />
+          </div>
+          <div className="input-container">
+            <button
+              className="button-save"
+              onClick={this._onSave}
+            >
+              Save
+                </button>
+          </div> */}
+          </div>
+        </div>
       </div>
     );
   }
