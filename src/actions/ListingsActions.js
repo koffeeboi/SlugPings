de09e2ed5244aefb137db487eddf8ba8f19c1691
Aaron <aahchn@gmail.com>
2018-03-04 22:42:42 -0800

    fixed listing actions

--- src/actions/ListingsActions.js
+++ src/actions/ListingsActions.js
@@ -7,3 +7,16 @@ export const showListings = () => ({
 export const hideListings = () => ({
   type: types.LISTINGS_HIDE,
 });
+
+export const showListingInfo = () => ({
+  type: types.LISTING_INFO_SHOW,
+})
+
+export const hideListingInfo = () => ({
+  type: types.LISTING_INFO_HIDE,
+})
+
+export const setListingInfoID = (id) => ({
+  type: types.LISTING_INFO_SET_ID,
+  payload: id,
+})
