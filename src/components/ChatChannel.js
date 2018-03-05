7a12994beb734b3994d6ba490f0dde685a093b01
Huey <jasperjeng@gmail.com>
2018-03-04 18:39:21 -0800

    Removed unused constructor

--- src/components/ChatChannel.js
+++ src/components/ChatChannel.js
@@ -3,10 +3,6 @@ import React, { Component } from 'react';
 import "../styles/ChatChannel.css";
 
 class ChatChannel extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   componentsDidMount() {
   }
 
