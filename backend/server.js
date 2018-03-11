178dd90b8002782cbcbf53315a3b51f9df221e07
Huey <jasperjeng@gmail.com>
2018-03-10 22:29:54 -0800

    Tweaked bodyParser settings for our app to receive JSON. Wasn't necessary, so maybe need to revert.

--- backend/server.js
+++ backend/server.js
@@ -23,15 +23,16 @@ db.on("connected", () => {
 });
 
 //now we should configure the APi to use bodyParser and look for JSON data in the body
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //To prevent errors from Cross Origin Resource Sharing, we will set our headers to allow CORS with middleware like so:
 app.use(function (req, res, next) {
+  res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
 
   //and remove cacheing so we get the most recent comments
   res.setHeader('Cache-Control', 'no-cache');
