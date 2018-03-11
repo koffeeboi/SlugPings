cb6ca2002932d350228d8d60d6be92e4664cc243
Huey <jasperjeng@gmail.com>
2018-03-11 03:46:19 -0700

    Moved retrieveDatabaseMarkers to dispatch actions

---
+++ src/actions/DatabaseAction.js
@@ -0,0 +1,36 @@
+import * as types from "../constants/DatabaseConstants";
+
+import { getMarkers } from "../utils/utils";
+
+export const setDatabaseMarkersInStore = (markers) => ({
+  type: types.DB_SET_MARKERS,
+  payload: markers,
+});
+
+export const retrieveDatabaseMarkers = () => (dispatch, getState) => {
+  getMarkers()
+    .then(async (resp) => {
+      let msg = await resp.json();
+
+      if (msg.status != 200)
+        throw Error("Unable to get markers from database");
+
+      //console.log(msg);
+      console.log("Succesfully received markers from database");
+
+      const { map } = getState();
+      const { markers } = map;
+
+      // Remove any duplicates such as own markers O(n^2)
+      let dbMarkers = msg.markers.filter(dbMarker => {
+        for (let marker of markers) {
+          if (marker.id == dbMarker.id)
+            return false;
+        }
+        return true;
+      });
+
+      dispatch(setDatabaseMarkersInStore(dbMarkers));
+    })
+    .catch(error => console.log(error));
+};
