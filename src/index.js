72e3789b98ce1d7b092583c320f9db184d86e096
Huey <jasperjeng@gmail.com>
2018-03-11 03:46:57 -0700

    Commented out polling code, which was moved to into dispatch actions

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import './styles/index.css';
 
 import configureStore from './store/configureStore';
 import RootContainer from "./containers/RootContainer";
+import { setDatabaseMarkersInStore } from "./actions/DatabaseAction";
+import { getMarkers } from "./utils/utils";
 
 let store = configureStore();
 ReactDOM.render(
@@ -16,6 +18,38 @@ ReactDOM.render(
   </Provider>
   , document.getElementById('root')
 );
+
+// Poll for database markers and set in store
+const retrieveDatabaseMarkers = () => {
+  setInterval(async () => {
+    getMarkers()
+      .then(async (resp) => {
+        let msg = await resp.json();
+
+        if (msg.status != 200)
+          throw Error("Unable to get markers from database");
+
+        //console.log(msg);
+        console.log("Succesfully received markers from database");
+
+        const { map } = store.getState();
+        const { markers } = map;
+
+        // Remove any duplicates such as own markers O(n^2)
+        let dbMarkers = msg.markers.filter(dbMarker => {
+          for (let marker of markers) {
+            if (marker.id == dbMarker.id)
+              return false;
+          }
+          return true;
+        });
+
+        store.dispatch(setDatabaseMarkersInStore(dbMarkers));
+      })
+      .catch(error => console.log(error));
+  }, 2000);
+};
+// retrieveDatabaseMarkers();
 
 // const retrieveData = () => {
 //   setInterval(async () => {
