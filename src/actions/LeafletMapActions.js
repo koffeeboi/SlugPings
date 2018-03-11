0b92256d01d2f71b941209bf33c6715ad513f718
Huey <jasperjeng@gmail.com>
2018-03-10 22:31:11 -0800

    Added functions setMarkers and removeMarker.

--- src/actions/LeafletMapActions.js
+++ src/actions/LeafletMapActions.js
@@ -8,9 +8,10 @@ export const hideMarkerModal = () => ({
   type: types.MAP_HIDE_MARKER_MODAL
 })
 
-export const addMarker = ({ lat, lng, title, loc, startTime, endTime, moreInfo }) => ({
+export const addMarker = ({ id, lat, lng, title, loc, startTime, endTime, moreInfo }) => ({
   type: types.MAP_ADD_MARKER,
   payload: {
+    id: id,
     lat: lat,
     lng: lng,
     title: title,
@@ -20,6 +21,23 @@ export const addMarker = ({ lat, lng, title, loc, startTime, endTime, moreInfo }
     moreInfo: moreInfo,
   },
 });
+
+export const setMarkers = (markers) => ({
+  type: types.MAP_SET_MARKERS,
+  payload: markers,
+})
+
+export const removeMarker = (id) => (dispatch, getState) => {
+   const { map } = getState();
+   const newMarkers = [...map.markers];
+   for (let i = 0; i < newMarkers.length; i++) {
+     if (newMarkers[i].id == id) {
+       // Remove the marker at index i
+       newMarkers.splice(i, 1);
+     }
+   }
+   dispatch(setMarkers(newMarkers));
+};
 
 export const setLastLoc = ({ lat, lng }) => ({
   type: types.MAP_SET_LAST_LOC,
