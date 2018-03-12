0329d243d092b2ffbee99855346f5c6c946a8ecf
Huey <jasperjeng@gmail.com>
2018-03-11 17:24:32 -0700

    Displays messages from other clients.

--- src/components/ChatChannel.js
+++ src/components/ChatChannel.js
@@ -1,9 +1,59 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client';
 
 import "../styles/ChatChannel.css";
 
+import ChatChannelMessage from "../components/ChatChannelMessage";
+
 class ChatChannel extends Component {
-  componentsDidMount() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      channelID: this.props.chat.channelID,
+      username: "",
+      message: "",
+      messages: [],
+    }
+
+    this._onClose = this._onClose.bind(this);
+    this._addMessage = this._addMessage.bind(this);
+    this._sendMessage = this._sendMessage.bind(this);
+    this._createMessages = this._createMessages.bind(this);
+
+    this.socket = io("localhost:3001");
+    this.socket.on("RECEIVE_MESSAGE", (data) => {
+      this._addMessage(data);
+    });
+  }
+
+  _onClose(e) {
+    e.stopPropagation();
+    const { hideChatChannel } = this.props;
+    hideChatChannel();
+  }
+
+  _addMessage(data) {
+    this.setState({ messages: [...this.state.messages, data] });
+  }
+
+  _sendMessage(event) {
+    event.preventDefault();
+    this.socket.emit("SEND_MESSAGE", {
+      username: this.state.username,
+      channelID: this.state.channelID,
+      message: this.state.message,
+    });
+    this.setState({ message: "" });
+  }
+
+  _createMessages() {
+    return this.state.messages.map((message, index) => {
+      let key = `${message}-${index}`;
+      return (
+        <ChatChannelMessage key={key} message={message} />
+      )
+    });
   }
 
   render() {
@@ -11,8 +61,39 @@ class ChatChannel extends Component {
 
     return (
       <div className="chat">
-        <div className="chat-header">
-          <span>{chat.channelID}</span>
+        <div className="chat-content">
+          <span
+            className="close"
+            onClick={this._onClose}
+          >
+            &times;
+          </span>
+          <div className="chat-header">
+            Chat for {chat.title}
+          </div>
+          <div className="chat-body">
+            {this._createMessages()}
+            <input
+              type="text"
+              placeholder="username"
+              className="input-username"
+              value={this.state.username}
+              onChange={e => this.setState({ username: e.target.value })}
+            />
+            <input
+              type="text"
+              placeholder="Message"
+              className="input-message"
+              value={this.state.message}
+              onChange={e => this.setState({ message: e.target.value })}
+            />
+            <button
+              className="send-btn"
+              onClick={this._sendMessage}
+            >
+              Send
+            </button>
+          </div>
         </div>
       </div>
     );
---
+++ src/components/ChatChannelMessage.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import "../styles/ChatChannelMessage.css";
+
+class ChatChannelMessage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { message } = this.props;
+    return (
+      <div className="cc-msg-container">
+        <span className="cc-username">{message.username}: </span>
+        <span className="cc-msg">{message.message}</span>
+      </div>
+    )
+  }
+}
+
+export default ChatChannelMessage;
