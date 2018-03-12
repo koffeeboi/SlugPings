78e30069d0ae5d7503c4fd4b8124d7efbbd16758
Huey <jasperjeng@gmail.com>
2018-03-11 17:24:55 -0700

    Basic layout for displaying information for an event

--- src/components/ListingInfo.js
+++ src/components/ListingInfo.js
@@ -3,14 +3,48 @@ import React, { Component } from 'react';
 import "../styles/ListingInfo.css";
 
 class ListingInfo extends Component {
-  componentsDidMount() {
+  constructor(props) {
+    super(props);
+
+    this._onClose = this._onClose.bind(this);
+  }
+
+  _onClose(event) {
+    event.stopPropagation();
+    const { hideListingInfo } = this.props;
+    hideListingInfo();
   }
 
   render() {
+    const { info } = this.props;
+
     return (
-      <div className="listing-info">
+      <div className="listing-info-content">
+        <span
+          className="listing-info-close"
+          onClick={this._onClose}
+        >
+          &times;
+        </span>
         <div className="listing-info-header">
-          Listing information
+          <span>Information</span>
+        </div>
+        <div className="listing-info-body">
+          <div className="listing-info-item">
+            Event name: {info.title}
+          </div>
+          <div className="listing-info-item">
+            Location: {info.loc}
+          </div>
+          <div className="listing-info-item">
+            Start time: {info.startTime}
+          </div>
+          <div className="listing-info-item">
+            End time: {info.endTime}
+          </div>
+          <div className="listing-info-item">
+            Description: {info.moreInfo}
+          </div>
         </div>
       </div>
     );
